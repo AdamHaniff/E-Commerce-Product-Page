c1c1413b26587a94f9665f3d7bf587ab2f2f4e87
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-09-16 16:49:18 -0400

    Made headerCartContainer pulse

--- js/main.js
+++ js/main.js
@@ -8,22 +8,42 @@ const quantityNumber = document.querySelector(".quantity__number");
 const addToCartBtn = document.querySelector(".add-cart-btn");
 const quantityContainer = document.querySelector(".quantity");
 const headerCartQuantity = document.querySelector(".header__cart-quantity");
+const headerCartContainer = document.querySelector(".header__cart-container");
 
-// Initialize 'quantity' with a value of 0
+// Initialize 'quantity' and 'totalCartQuantity' with a value of 0
 let quantity = 0;
+let totalCartQuantity = 0;
 
 // FUNCTIONS
-function makeElementShake(element) {
-  element.classList.add("shake");
+function makeQuantityContainerShake() {
+  quantityContainer.classList.add("shake");
 
   // Remove 'shake' class after animation ends
-  element.addEventListener("animationend", function () {
-    element.classList.remove("shake");
+  quantityContainer.addEventListener("animationend", function () {
+    quantityContainer.classList.remove("shake");
   });
 }
 
-// EVENT LISTENERS
-quantityPlusBtn.addEventListener("click", function (e) {
+function scrollToTopOfPage() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+function displayHeaderCartQuantity() {
+  // Update value of 'totalCartQuantity'
+  totalCartQuantity += quantity;
+
+  // Update text content of 'headerCartQuantity' to the value of 'totalCartQuantity'
+  headerCartQuantity.textContent = totalCartQuantity;
+
+  // Make 'headerCartQuantity' visible
+  headerCartQuantity.classList.remove("hidden");
+}
+
+// EVENT LISTENER CALLBACK FUNCTIONS
+function handleQuantityPlusBtnClick(e) {
   const quantityPlusBtn = e.target.closest(".quantity__plus");
   if (!quantityPlusBtn) return;
 
@@ -32,9 +52,9 @@ quantityPlusBtn.addEventListener("click", function (e) {
 
   // Update text content of 'quantityNumber'
   quantityNumber.textContent = quantity;
-});
+}
 
-quantityMinusBtn.addEventListener("click", function (e) {
+function handleQuantityMinusBtnClick(e) {
   const quantityMinusBtn = e.target.closest(".quantity__minus");
   if (!quantityMinusBtn) return;
 
@@ -46,26 +66,26 @@ quantityMinusBtn.addEventListener("click", function (e) {
 
   // Update text content of 'quantityNumber'
   quantityNumber.textContent = quantity;
-});
+}
+
+// EVENT LISTENERS
+quantityPlusBtn.addEventListener("click", handleQuantityPlusBtnClick);
+quantityMinusBtn.addEventListener("click", handleQuantityMinusBtnClick);
 
 addToCartBtn.addEventListener("click", function (e) {
   const addToCartBtn = e.target.closest(".add-cart-btn");
   if (!addToCartBtn) return;
 
   if (quantity === 0) {
-    makeElementShake(quantityContainer);
+    makeQuantityContainerShake(quantityContainer);
     return;
   }
 
   // If 'quantity' is more than 0, scroll smoothly to the top of page
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+  scrollToTopOfPage();
 
-  // Make 'headerCartQuantity' visible
-  headerCartQuantity.classList.remove("hidden");
+  displayHeaderCartQuantity();
 
-  // Update text content of 'headerCartQuantity' to the value of 'quantity'
-  headerCartQuantity.textContent = quantity;
+  // Make 'headerCartContainer' pulse
+  headerCartContainer.classList.add("pulse");
 });
