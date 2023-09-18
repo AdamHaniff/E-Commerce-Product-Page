cb746c7f7c4b34573520fdd2f574cf903010bcaa
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-09-18 16:46:53 -0400

    Created event listener for headerCartBtn

--- js/main.js
+++ js/main.js
@@ -8,7 +8,13 @@ const quantityNumber = document.querySelector(".quantity__number");
 const addToCartBtn = document.querySelector(".add-cart-btn");
 const quantityContainer = document.querySelector(".quantity");
 const headerCartQuantity = document.querySelector(".header__cart-quantity");
-const headerCartContainer = document.querySelector(".header__cart-container");
+const headerCartBtn = document.querySelector(".header__cart-btn");
+const cart = document.querySelector(".cart");
+const cartEmptyContainer = document.querySelector(".cart__empty");
+const cartFullContainer = document.querySelector(".cart__full");
+const cartQuantity = document.querySelector(".cart__product-quantity");
+const cartTotalPrice = document.querySelector(".cart__total-price");
+const cartDeleteBtn = document.querySelector(".cart__delete-btn");
 
 // Initialize 'quantity' and 'totalCartQuantity' with a value of 0
 let quantity = 0;
@@ -81,11 +87,53 @@ function handleAddToCartBtnClick(e) {
   scrollToTopOfPage();
   displayHeaderCartQuantity();
 
-  // Make 'headerCartContainer' pulse and give cart icon a color change animation
-  headerCartContainer.classList.add("pulse");
+  // Make 'headerCartBtn' pulse and give cart icon a color change animation
+  headerCartBtn.classList.add("pulse");
 }
 
 // EVENT LISTENERS
 quantityPlusBtn.addEventListener("click", handleQuantityPlusBtnClick);
 quantityMinusBtn.addEventListener("click", handleQuantityMinusBtnClick);
 addToCartBtn.addEventListener("click", handleAddToCartBtnClick);
+
+headerCartBtn.addEventListener("click", function (e) {
+  const headerCartBtn = e.target.closest(".header__cart-btn");
+  if (!headerCartBtn) return;
+
+  // If 'headerCartBtn' has already been clicked
+  if (headerCartBtn.classList.contains("clicked")) {
+    // Change cart icon back to gray
+    headerCartBtn.classList.remove("clicked");
+
+    // Hide the cart
+    cart.classList.add("hidden");
+    return;
+  }
+
+  // Check if the cart is empty or not
+  if (totalCartQuantity === 0) {
+    // Turn the header cart icon black
+    headerCartBtn.classList.add("clicked");
+
+    // Display the cart and show the user that their cart is empty
+    cart.classList.remove("hidden");
+  } else {
+    // Stop the 'headerCartBtn' from pulsing
+    headerCartBtn.classList.remove("pulse");
+
+    // Turn the header cart icon black
+    headerCartBtn.classList.add("clicked");
+
+    // Display the cart
+    cart.classList.remove("hidden");
+
+    // Update text content of 'cartQuantity' and 'cartTotalPrice'
+    const totalPrice = `$${(125 * totalCartQuantity).toFixed(2)}`;
+    cartQuantity.textContent = totalCartQuantity;
+    cartTotalPrice.textContent = totalPrice;
+
+    // Show the user what is in their cart
+    cartEmptyContainer.classList.add("hidden");
+    cartFullContainer.classList.remove("hidden");
+  }
+});
