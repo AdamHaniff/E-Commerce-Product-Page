e8d2ff98d5e2d8c39dd077b7dbee326e743fa9cc
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-09-20 16:47:35 -0400

    Styled header in 320px-374px media query

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,6 +3,7 @@ const menuBtn = document.querySelector(".header__menu-btn");
 const sidebar = document.querySelector(".sidebar");
 const sidebarCloseBtn = document.querySelector(".sidebar__close-btn");
 const sidebarLinks = document.querySelector(".sidebar__links");
+const sidebarOverlay = document.querySelector(".sidebar__overlay");
 
 // EVENT LISTENER CALLBACK FUNCTIONS
 function handleMenuBtnClick(e) {
@@ -30,7 +31,18 @@ function handleSidebarLinkClick(e) {
   }
 }
 
+function handleSidebarOverlayClick(e) {
+  const sidebarOverlay = e.target;
+
+  // Make sure the element that is being clicked is the 'sidebarOverlay'
+  if (!sidebarOverlay.classList.contains("sidebar__overlay")) return;
+
+  // When the 'sidebarOverlay' is clicked, hide the sidebar
+  sidebar.classList.toggle("sidebar--open");
+}
+
 // EVENT LISTENERS
 menuBtn.addEventListener("click", handleMenuBtnClick);
 sidebarCloseBtn.addEventListener("click", handleSidebarCloseBtnClick);
 sidebarLinks.addEventListener("click", handleSidebarLinkClick);
+sidebarOverlay.addEventListener("click", handleSidebarOverlayClick);
