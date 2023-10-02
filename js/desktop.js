a7c5c341b14ae647f48495bb7592f36102f34c9b
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-10-02 19:06:15 -0400

    Created handleGalleryThumbnailsHover function

--- js/desktop.js
+++ js/desktop.js
@@ -4,9 +4,11 @@ const galleryThumbnails = document.querySelector(".gallery__thumbnails");
 const galleryThumbnailContainers = document.querySelectorAll(
   ".gallery__thumbnail-container"
 );
+const lightbox = document.querySelector(".lightbox");
 
-galleryThumbnails.addEventListener("mouseover", function (e) {
-  // Check if the event target is the parent container itself
+// EVENT LISTENER CALLBACK FUNCTION
+function handleGalleryThumbnailsHover(e) {
+  // Check if the event target is 'galleryThumbnails'
   if (e.target === this) return;
 
   // Check if the event target or its parent has the class 'gallery__thumbnail-container'
@@ -32,6 +34,28 @@ galleryThumbnails.addEventListener("mouseover", function (e) {
       const thumbnailImage = target.querySelector(".gallery__thumbnail");
       galleryImage.src = thumbnailImage.src;
 
+      return;
+    }
+
+    // Move up to the parent element
+    target = target.parentElement;
+  }
+}
+
+// EVENT LISTENERS
+galleryThumbnails.addEventListener("mouseover", handleGalleryThumbnailsHover);
+
+galleryThumbnails.addEventListener("click", function (e) {
+  // Check if the event target is 'galleryThumbnails'
+  if (e.target === this) return;
+
+  // Check if the event target or its parent has the class 'gallery__thumbnail-container'
+  let target = e.target;
+
+  while (target !== this) {
+    if (target.classList.contains("gallery__thumbnail-container")) {
+      // Display lightbox
+      lightbox.classList.add("lightbox--open");
       return;
     }
 
